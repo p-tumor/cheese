96ff2142309a6a91568803c874114dad55fa21dc
BT_2N8_04 <hello>
2023-06-05 15:30:04 -0400

    one step closer man

--- src/Game.java
+++ src/Game.java
@@ -3,32 +3,43 @@ import java.awt.*;
 
 public class Game extends JPanel {
     Player p1, p2;
-    Card [] p1Hand = new Card[15];
+    Card [][] p1Hand;
     public Game(Player p1, Player p2) {
+        p1Hand = new Card[][]{
+                {null},
+                {null,null},
+                {null,null,null},
+                {null,null,null,null},
+                {null,null,null,null,null}
+        };
         this.p1 = p1;
         this.p2 = p2;
-        for(int i = 0; i < p1Hand.length; i++){
-            System.out.println(p1.getPlayerHand().size());
-            //p1Hand[i] = new ImageIcon(p1.getCardsAtPlay()[i].getCARD_FRONT()).getImage();
-            p1Hand[i] = p1.getCardsAtPlay()[i];
+        for(Card[] pile: p1Hand) {
+            for (int i = 0; i < pile.length; i++) {
+                System.out.println(p1.getPlayerHand().size());
+                //p1Hand[i] = new ImageIcon(p1.getCardsAtPlay()[i].getCARD_FRONT()).getImage();
+                pile[i] = p1.getCardsAtPlay()[i];
+            }
         }
-
+        System.out.println(p1Hand);
     }
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
-        for(int i = 0; i < 7; i++){
-            switch (i){
-                case 0 ->{
-                    g2D.drawImage(p1Hand[i].getCARD_FRONT(),75,400,null);
-                }
-                case (1) ->{
+        for (Card[] pile : p1Hand) {
+            for (int i = 0; i < pile.length; i++) {
+                switch (i) {
+                    case 0 -> {
+                        g2D.drawImage(pile[i].getCARD_FRONT(), 75, 400, null);
+                    }
+                    case (1) -> {
 
-                    g2D.drawImage(p1Hand[i].getCARD_FRONT(),150,400,null);
-                }
-                case (2)->{
-                    g2D.drawImage(p1Hand[i].getCARD_FRONT(),152,400,null);
-                }
+                        g2D.drawImage(pile[i].getCARD_FRONT(), 150, 400, null);
+                    }
+                    case (2) -> {
+                        g2D.drawImage(pile[i].getCARD_FRONT(), 152, 400, null);
+
+                    }
                 /*case(3)
                 case(4)
                 case(5)
@@ -43,8 +54,8 @@ public class Game extends JPanel {
                 case(14)
 
                  */
+                }
             }
         }
     }
-
 }
